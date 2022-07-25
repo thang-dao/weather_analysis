3d3b4cbcebd2f9fcc304c13d4eb4300845292d6d
Ubuntu <Ubuntu@vieon.com>
2022-07-25 15:18:33 +0000

    #update new model

--- main.py
+++ main.py
@@ -8,21 +8,31 @@ from utils import read_json
 load_dotenv(dotenv_path='config/.env')
 
 logger = logging.getLogger('Weather-Prediction')
+RAINY_LABEL = ['Light drizzle','Light rain','Light rain shower', 'Patchy light drizzle', 'Patchy light rain',
+             'Patchy light rain with thunder','Patchy rain possible','Heavy rain','Heavy rain at times',
+             'Moderate or heavy rain shower','Moderate rain', 'Moderate rain at times', 'Overcast','Torrential rain shower']
+
 
 class WeatherPrediction:
     def __init__(self):
         self.base_input = read_json(os.getenv('BASE_INPUT'))
-        self.model_dict = os.getenv('MODEL_DICT')
+        self.model_dict = read_json(os.getenv('MODEL_DICT'))
+        self.class_name = read_json(os.getenv('CLASS_NAME'))
         self.models = dict()
-        for k,v in self.model_dict:
-            self.models[k] = self._load_model(v)
-    
+        for model_inf in self.model_dict:
+            model_ins, name = self._load_model(model_inf)
+            self.models[name] = model_ins
+             
     def _load_model(self, model_inf):
         logger.info('Loading model: {}'.format(model_inf['name']))
         model = sm.load(model_inf['dir'])
-        return model
-        
+        return model, model_inf['type']
+
     def preprocess(self, x):
+        if x['Weather'] in RAINY_LABEL:
+            x['Weather'] = 'rainy'
+        else:
+            x['Weather'] = 'sunny'
         input_model = self.base_input.copy()
         for k,v in x.items():
             if k in input_model.keys():
@@ -34,12 +44,19 @@ class WeatherPrediction:
                     input_model['Month_'+str(v)] = 1
                 elif k == 'Weather':
                     input_model['Weather_'+str(v)] = 1
-        df_model = pd.DataFrame(input_model)
+        df_model = pd.DataFrame([input_model])
         return df_model
-            
-    @lru_cache(maxsize=16)
+
     def predict(self, inputs, model_type=0):
         model_input = self.preprocess(inputs)
+        assert model_input.shape == (1,33)
         y = self.models[model_type].predict(model_input)
         pred = list(map(round, y))
-        return pred
+        return self.class_name[str(pred[0])]
+    
+    
+if __name__=='__main__':
+    predictor = WeatherPrediction()
+    test_sample = read_json('config/test_sample.json')
+    pred = predictor.predict(test_sample)
+    print(pred)
